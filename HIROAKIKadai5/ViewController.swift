abbecf107d28c69f7b895e01cea4781841b8b6a7
hirokaissh <hirokai.saito1014@gmail.com>
2021-11-08 19:04:32 +0900

    TextFieldのアウトレット接続

--- HIROAKIKadai5/ViewController.swift
+++ HIROAKIKadai5/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var num1TextField: UITextField!
+    @IBOutlet private weak var num2TextField: UITextField!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()

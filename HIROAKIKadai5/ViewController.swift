af391bd44d78d2f60b728d5b6b25d0d251029182
hirokaissh <hirokai.saito1014@gmail.com>
2021-11-08 19:47:34 +0900

    キーボードタイプをNumberPadに限定

--- HIROAKIKadai5/ViewController.swift
+++ HIROAKIKadai5/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        num1TextField.keyboardType = .numberPad
+        num2TextField.keyboardType = .numberPad
     }
 
     @IBAction private func calculationButton(_ sender: Any) {
